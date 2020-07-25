4a71570ea4ee811e8903e84aa5b6cf92e91eb88b
Hosomi <surendaotoko@gmail.com>
2020-07-25 14:39:45 +0100

    Begin Phys Component

---
+++ Scarle2020-master/Scarle2020/Unit.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ImageGO2D.h"
+
+class Unit : public ImageGO2D
+{
+public:
+
+private:
+	static constexpr float UNIT_HEIGHT = 50;
+	static constexpr float UNIT_WIDTH = 30;
+};
+
